aaf449eb06f1d4dee30f9f1359ec7571c08d3da2
vn56c5j <junyi.Lin@walmart.com>
2023-10-19 14:42:10 -0400

    add one demo class

---
+++ src/main/java/com/walmart/vcsDemo/onboarding/common/util/DemoClass.java
@@ -0,0 +1,13 @@
+package com.walmart.vcsDemo.onboarding.common.util;
+
+import java.util.Random;
+
+public class DemoClass {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}

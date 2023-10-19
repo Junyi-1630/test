5cf44d0cb779ae22f8286eb4559f8056b1565be8
vn56c5j <junyi.Lin@walmart.com>
2023-10-19 14:44:18 -0400

    introduce code issue in demo class

--- src/main/java/com/walmart/vcsDemo/onboarding/common/util/DemoClass.java
+++ src/main/java/com/walmart/vcsDemo/onboarding/common/util/DemoClass.java
@@ -3,13 +3,6 @@ package com.walmart.vcsDemo.onboarding.common.util;
 import java.util.Random;
 
 public class DemoClass {
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;
-    }
 
     private String password = "password123"; // Issue: Hardcoded password
 

0011705563df5a3781ddb4e1786f32dabfec2b9d
vn56c5j <junyi.Lin@walmart.com>
2023-10-19 15:03:41 -0400

    resume to original demo class

--- src/main/java/com/walmart/vcsDemo/onboarding/common/util/DemoClass.java
+++ src/main/java/com/walmart/vcsDemo/onboarding/common/util/DemoClass.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 public class DemoClass {
 
-    private String password = "password123"; // Issue: Hardcoded password
 
     public int add(int a, int b) {
         if(true) { // Issue: Conditional always true
